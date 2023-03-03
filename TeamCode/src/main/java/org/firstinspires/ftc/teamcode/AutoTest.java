9d7c76603965e0358238b0cf9e0ec0c4b0f4802b
geg <70918804+rozam513@users.noreply.github.com>
2023-03-03 00:50:01 -0800

    Pawn to e4... (respond with your next commit) also yea its not good, but I wanted to go to sleep so have fun. Hopefully my documentation helps i doubt what I wrote will work fix the values I wont be there tommorow most likely. I probably forgot to do stuff. Also theres an error I want to go to sleep Ill fix it tmrw, or you will

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -13,36 +13,76 @@ public class AutoTest extends LinearOpMode {
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Sliders v4b = new Sliders(hardwareMap);
-
+        float robotLength = 0;
         Pose2d startPose = new Pose2d();
         TrajectorySequence traj = drive.trajectorySequenceBuilder(startPose)
-                .forward(50)
-                .turn(Math.toRadians(-90))
-                .forward(24)
+                //close the claw on the preloaded cone
                 .addTemporalMarker(() -> {
                     v4b.closeClaw();
                 })
                 .waitSeconds(1)
-                .back(24)
-                .turn(Math.toRadians(-45))
+                .forward(72 - robotLength/2)
+                .turn(Math.toRadians(-90))
                 .back(8)
-                .addTemporalMarker(() -> {
+                .addTemporalMarker(() ->{
                     v4b.highJunction();
                 })
                 .waitSeconds(3)
-                .addTemporalMarker(() -> {
+                .addTemporalMarker(() ->{
                     v4b.openClaw();
                 })
                 .waitSeconds(1)
                 .addTemporalMarker(() -> {
                     v4b.floor();
                 })
-                .waitSeconds(5)
+                .waitSeconds(3)
+                .forward(8)
+                //I dont even know if its right or left tbh or the length lets change
+                .strafeRight(12)
+                .back(24)
                 .build();
+        //like hell it will be 5
+        for(int i = 0; i < 5; i++) {
+            //dont worry about it
+            int finalI = i;
+            //putting this in a loop is it even halal? Will it work
+            TrajectorySequence trajLoop = drive.trajectorySequenceBuilder(startPose)
+                    .forward(50)
+                    .turn(Math.toRadians(-90))
+                    .forward(24)
+                    //I dont know if its neccesary to add 2 temporalmarkers but I saw you do it so I guess
+                    .addTemporalMarker(() -> {
+                        //these values are bullshit I just want to go to sleep also I think doing it with the slides is more optimal but your call
+                        v4b.motorGrouping(1 - (finalI *0.05));
+                    })
+                    .waitSeconds(2)
+                    .addTemporalMarker(() -> {
+                        v4b.closeClaw();
+                    })
+                    .waitSeconds(1)
+                    .back(24)
+                    .turn(Math.toRadians(-45))
+                    .back(8)
+                    .addTemporalMarker(() -> {
+                        v4b.highJunction();
+                    })
+                    .waitSeconds(3)
+                    .addTemporalMarker(() -> {
+                        v4b.openClaw();
+                    })
+                    .waitSeconds(1)
+                    .addTemporalMarker(() -> {
+                        v4b.floor();
+                    })
+                    .waitSeconds(5)
+                    .build();
+        }
 
 
         waitForStart();
         v4b.openClaw();
         drive.followTrajectorySequence(traj);
+        //do I have to do this multiple times if its looped. Also fix this error I want to go to sleep
+        drive.followTrajectorySequence(trajLoop);
     }
 }
